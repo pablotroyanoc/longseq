f4ac0619948a38caa6e3b7153833ba0cf617003a
Pablo <pablotroyanoc@gmail.com>
2026-02-26 13:07:30 +0100

    Filtro para las 6 muestras

---
+++ scripts/02_nanofilt_lsq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=02_nanofilt
+#SBATCH --output=logs/02_nanofilt_%j.out
+#SBATCH --error=logs/02_nanofilt_%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=02:00:00
+#SBATCH --qos=short
+
+module load anaconda
+conda activate nanoplot_env
+
+for i in {1..6}; do
+    NanoFilt -q 6 data/Muestra_${i}.fastq.gz > data/Muestra_${i}_filtered.fastq
+done
