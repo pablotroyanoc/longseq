a9406e6f92477f6c671ab1c2aca19d4e469096ff
Pablo <pablotroyanoc@gmail.com>
2026-02-26 10:39:31 +0100

    Añadida cabecera Slurm

--- scripts/00_prepro_lsq.sh
+++ scripts/00_prepro_lsq.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH --job-name=00_prepro_lsq
+#SBATCH --output=logs/00_prepro_lsq_%j.out
+#SBATCH --error=logs/00_prepro_lsq_%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=02:00:00
+#SBATCH --qos=short
 
 while read -r old_path new_id; do
 
