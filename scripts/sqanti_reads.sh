b08eb5f6539ed6a9d1ab65debb9b6f98ded99e0b
Pablo Troyano pablotroyanoc@gmail.com <patroy@master2.data.cpd>
2026-03-26 14:31:25 +0100

    fixed script

--- scripts/sqanti_reads.sh
+++ scripts/sqanti_reads.sh
@@ -2,20 +2,23 @@
 #SBATCH --job-name=SQ3_FINAL_OK
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
+#SBATCH --time=1:00:00
 #SBATCH --qos=short
 #SBATCH --output=sqanti_reads.out
-#SBATCH --error=sqanti_reads.err
-
+#SBATCH --error=sqanti_reads.out
+set -e
 
 module load anaconda
 conda activate sqanti3
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
+WD=/home/patroy/longseq
+
 python /home/patroy/Programs/SQANTI3/sqanti3_reads.py \
-  --refFasta /home/patroy/longseq/data/genome_reference/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
-  --refGTF /home/patroy/longseq/data/anotation/Homo_sapiens.GRCh38.115.chr.gtf \
-  --design design.csv \
-  -d ./ \
-  -o total_analysis_merged \
-  -t 8
+  --refFasta $WD/data/genome_reference/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
+  --refGTF $WD/data/anotation/Homo_sapiens.GRCh38.115.chr.gtf \
+  --design $WD/results/sqanti_reads/design.csv \
+  -d $WD/results/sqanti_reads/reads_SQ_QC/ \
+  -o $WD/results/sqanti_reads/sqanti_reads_v2 \
+  -t 8 --verbose
